4418b573c2d9d71cf0764bf42827fd47fda0e2fd
fahad <fahad_zafar.abdul_bari@bell.ca>
2023-09-08 14:09:34 -0400

    Better example

--- src/main/java/com/example/Application.java
+++
@@ -1,5 +0,0 @@
-public class Application {
-    public static void main(String[] args) {
-        // Spring Boot application logic here
-    }
-}
--- src/test/java/com/example/ApplicationTests.java
+++
@@ -1,3 +0,0 @@
-public class ApplicationTests {
-    // Test cases here
-}
